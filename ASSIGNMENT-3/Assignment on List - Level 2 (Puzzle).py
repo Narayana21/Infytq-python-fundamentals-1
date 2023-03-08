f4e6342896dafdc88cb0a03e33c0daf3441e85eb
Nimmalapudi Lakshmi Narayana <126308192+Narayana21@users.noreply.github.com>
2023-03-08 13:14:51 +0530

    Update Assignment on List - Level 2 (Puzzle).py

--- ASSIGNMENT-3/Assignment on List - Level 2 (Puzzle).py
+++ ASSIGNMENT-3/Assignment on List - Level 2 (Puzzle).py
@@ -15,3 +15,16 @@ def create_largest_number(number_list):
 number_list=[23,34,55]
 largest_number=create_largest_number(number_list)
 print(largest_number)
+
+#or it can also be done in this way
+
+def create_largest_number(number_list):
+    number_list.sort(reverse=True)
+    s=''
+    for i in number_list:
+        s=s+str(i)
+    return int(s)
+    #   print(i,sep='',end='')
+number_list=[67,97,55]
+largest_number=create_largest_number(number_list)
+print(largest_number)
